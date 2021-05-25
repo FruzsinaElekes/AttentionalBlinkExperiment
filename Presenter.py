29ce026e9edfbc1e1c2a535f606564db5dd1e28d
Fruzsina <elekes.fr@gmail.com>
2021-05-25 21:09:51 +0200

    Initialize window in Presenter

--- Presenter.py
+++ Presenter.py
@@ -4,15 +4,23 @@ class Presenter:
     
     def __init__ (self, config):
         self.config = config
-        
-        
+    
+    
+    def create_window(self):
+        self.myWin = visual.Window(
+            size = self.config["vars"]["monitor_size"], 
+            color = self.config["vars"]["backgr_color"], 
+            fullscr = True, 
+            units = self.config["vars"]["units"])
+        self.myWin.winHandle.set_mouse_visible(False)
+    
     
     def give_instruction(self, text):
-        pass
-        
+        print("give inst")
+    
+    
     def ask_for_participant_number(self):
         exp_info =  {'participant_number':''}
         dlg = gui.DlgFromDict(dictionary = exp_info, title = 'AB')
-        while dlg.OK == False:
-            dlg = gui.DlgFromDict(dictionary = exp_info, title = 'AB')
+        if dlg.OK == False:core.quit()
         return int(exp_info['participant_number'])
