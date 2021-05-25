1198eb6a5c07e2df7aebff220f476cceff0ce58c
Fruzsina <elekes.fr@gmail.com>
2021-05-25 20:52:47 +0200

    Refactor Participant

--- Participant.py
+++ Participant.py
@@ -1,8 +1,7 @@
 
 class Participant:
     
-    def __init__(self, p_no, config):
+    def __init__(self, p_no, k1, k2):
         self.participant_number = p_no
-        k1, k2 = config["vars"]["part_keys"]
         self.yes_key = k1 if p_no % 2 == 1 else k2
         self.no_key = k2 if p_no % 2 == 1 else k1
