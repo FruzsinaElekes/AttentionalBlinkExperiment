6e421de4aeb3c5217c679c0f82a7126c8415849f
Fruzsina <elekes.fr@gmail.com>
2021-05-25 20:46:40 +0200

    Add Participant, with part_no, yes and no keys

--- Participant.py
+++ Participant.py
@@ -1,8 +1,8 @@
 
 class Participant:
     
-    def __init__(self, p_no):
+    def __init__(self, p_no, config):
         self.participant_number = p_no
         k1, k2 = config["vars"]["part_keys"]
-        self.yes_key = k1 if pno % 2 == 1 else k2
-        self.no_key = k2 if pno % 2 == 1 else k1
+        self.yes_key = k1 if p_no % 2 == 1 else k2
+        self.no_key = k2 if p_no % 2 == 1 else k1
--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 import yaml
 from ABTrialClass import ABTrial 
+from ABExperiment import ABExperiment
+from Presenter import Presenter
+from Participant import Participant
 
 def run_experiment():
     config = yaml.safe_load(open("config.yaml"))
-    trial = ABTrial(1, 3, 2, config)
+    presenter = Presenter(config)
+    participant = Participant(presenter.ask_for_participant_number(), config)
+    print(participant.yes_key)
+    print(participant.participant_number)
+
+    exp = ABExperiment(config)
 
 if __name__ == "__main__":
     run_experiment()
