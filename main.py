3ae01d367e729d67131b04c35abc50378a761966
Fruzsina <elekes.fr@gmail.com>
2021-05-29 12:35:21 +0200

    Initialize ABExperiment in main and run

---
+++ main.py
@@ -0,0 +1,20 @@
+import yaml
+from ABTrialClass import ABTrial 
+from ABExperiment import ABExperiment
+from Presenter import Presenter
+from Participant import Participant
+from FileWriter import FileWriter
+
+def main():
+    config = yaml.safe_load(open("config.yaml"))
+    presenter = Presenter(config)
+    participant = Participant(
+        presenter.ask_for_participant_number(), 
+        *config["vars"]["part_keys"])
+    file_writer = FileWriter()
+
+    exp = ABExperiment(participant, presenter, file_writer, config)
+    exp.run()
+
+if __name__ == "__main__":
+    main()
