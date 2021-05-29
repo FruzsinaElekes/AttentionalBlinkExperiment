c73b99a73ced65803726ffb652f12d85a6937db4
Fruzsina <elekes.fr@gmail.com>
2021-05-29 10:09:41 +0200

    Add loop to iterate over trial_list in ABExperiment

--- ABExperiment.py
+++ ABExperiment.py
@@ -7,7 +7,7 @@ class ABExperiment:
         self.participant = participant
         self.presenter = presenter
         self.config = config
-        self.create_trial_list()
+        self.trial_list = self.create_trial_list()
         
         
     def create_trial_list(self):
@@ -22,6 +22,19 @@ class ABExperiment:
         corr_detection = [self.participant.yes_key if i == 1 else self.participant.no_key for i in x_present]
         combined = list(zip(x_present, T1_positions, T2_lags, corr_detection))
         random.shuffle(combined)
-        self.trial_list = [ ABTrial(*item, self.config) for item in combined ]
+        return [ ABTrial(*item, self.config) for item in combined ]
+        
+    
+    def run(self):
+        self.presenter.create_visual_objects()
+        
+        for t, trial in enumerate(self.trial_list): 
+            if t == 0:
+                self.presenter.instruct_waitkey(self.config["inst"]["start_exp"])
+            elif t == self.config["vars"]["total_trial_number"] / 2:
+                self.presenter.instruct_waittime_thenkey(self.config["inst"]["break"], 60)
+            
+            print(t)
+        
         
         
