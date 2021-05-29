7afcf08040d34e7df078e2ce41f812700d48be3f
Fruzsina <elekes.fr@gmail.com>
2021-05-29 09:53:19 +0200

    Add instruct_waitkey(str) method to Presenter

--- Presenter.py
+++ Presenter.py
@@ -1,4 +1,4 @@
-from psychopy import visual, gui
+from psychopy import visual, gui, core, event
 
 class Presenter: 
     
@@ -6,17 +6,32 @@ class Presenter:
         self.config = config
     
     
-    def create_window(self):
+    def create_visual_objects(self):
         self.myWin = visual.Window(
             size = self.config["vars"]["monitor_size"], 
+            pos = [0,0],
             color = self.config["vars"]["backgr_color"], 
             fullscr = True, 
             units = self.config["vars"]["units"])
         self.myWin.winHandle.set_mouse_visible(False)
+        
+        self.instructions = visual.TextStim(
+            self.myWin, 
+            wrapWidth = self.config["vars"]["wrap_width"], 
+            height = self.config["vars"]["text_height"], 
+            color = self.config["vars"]["text_color"], 
+            text = "")
     
     
-    def give_instruction(self, text):
-        print("give inst")
+    def instruct_waitkey(self, text):
+        self.instructions.setText(text)
+        self.instructions.draw()
+        self.myWin.flip()
+        keys = []
+        #while len(keys) == 0:
+        keys = event.waitKeys(keyList = ['space', 'escape'])
+        if keys[0] == 'escape':
+            core.quit()
     
     
     def ask_for_participant_number(self):
