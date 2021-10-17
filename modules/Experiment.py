59c272cb8590f0410ca64fea96795660eff02c27
FruzsinaElekes <elekes.fr@gmail.com>
2021-10-17 13:50:18 +0200

    Refactor experiment

--- modules/Experiment.py
+++ modules/Experiment.py
@@ -14,12 +14,10 @@ class Experiment:
     def create_trial_list(self):
         T1_0, T1_1, T1_2, T1_3 = self.config["vars"]["T1_pos"]
         total = self.config["vars"]["total_trial_number"]
-        x_present = [1] * int(total / 2) + [0] * int(total / 2)
-        T1_positions = ([T1_0] * int(total/8) 
-            + [T1_1] * int(total/8) 
-            + [T1_2] * int(total/8) 
-            + [T1_3] * int(total/8)) * 2
-        T2_lags = self.config["vars"]["T2_lag"] * int(total / 8) + [None] * int(total / 2)
+        half, eighth = int(total/2), int(total/8)
+        x_present = [1] * half + [0] * half
+        T1_positions = ([T1_0] * eighth + [T1_1] * eighth + [T1_2] * eighth + [T1_3] * eighth) * 2
+        T2_lags = self.config["vars"]["T2_lag"] * eighth + [None] * half
         corr_detection = [self.participant.yes_key if i == 1 else self.participant.no_key for i in x_present]
         combined = list(zip(x_present, T1_positions, T2_lags, corr_detection))
         random.shuffle(combined)
